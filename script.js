716ef458f3c30a866249fa862f566c7511ba8020
akshataamburayakoodagi-dotcom <akshataamburayakoodagi@gmail.com>
2026-04-05 09:48:22 -0400

    MAC
    sign out button added

--- script.js
+++ script.js
@@ -85,18 +85,18 @@ const adminPassword = "Admin@123";
 // const userPassword = "User@123";
 
 
-  // Pattern for Gmail validation
-  // Must be a proper Gmail ending with @gmail.com
-  const userEmail = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+// Pattern for Gmail validation
+// Must be a proper Gmail ending with @gmail.com
+const userEmail = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 
-  // Pattern for password validation
-  // Must contain:
-  // at least 1 letter
-  // at least 1 number
-  // at least 1 special character
-  // minimum 6 characters
-  const userPassword =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{6,}$/;
+// Pattern for password validation
+// Must contain:
+// at least 1 letter
+// at least 1 number
+// at least 1 special character
+// minimum 6 characters
+const userPassword =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{6,}$/;
 
 
 // Run when form is submitted
@@ -120,15 +120,16 @@ form.addEventListener("submit", function (event) {
     passwordError.textContent = "Password cannot be empty.";
     return;
   }
+
   // Save entered email so next page can use it
   localStorage.setItem("loggedInUser", enteredEmail);
-
   // Admin login
   if (enteredEmail === adminEmail && enteredPassword === adminPassword) {
     window.location.href = "admin.html";
   }
   // Normal user login
-  else{
+  else {
     window.location.href = "user.html";
   }
+
 });
